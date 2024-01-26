10f59640180dcfcf46c42b373c19026ef48c6b89
AlonsoParaiso <alonpahe2@gmail.com>
2024-01-26 14:01:46 +0100

    singleton

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public static GameManager instance; //el game manager controla las variables del juego y es accesible a todos
     public int TotalPoints { get { return totalPoints; } }
     private int totalPoints;
 
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;//se instancia el objecto
+            DontDestroyOnLoad(gameObject);// no se destruye entre cargas
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void PlusCoins(int puntosASumar)
     {
         totalPoints += puntosASumar;
